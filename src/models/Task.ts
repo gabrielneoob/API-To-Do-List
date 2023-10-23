4f19709cb3049200272d58d63a7b7fc4dd45b511
Gabriel Neoob <gabrielngc@gmail.com>
2023-10-23 09:37:41 -0300

    feat: Task model

--- src/models/Task.ts
+++ src/models/Task.ts
@@ -1,5 +1,22 @@
 import { Schema, connection, model, Model } from 'mongoose';
+import { TaskType } from '../types/task.type';
 
-const schema = new Schema({
-  
+const schema = new Schema<TaskType>({
+  task: {
+    type: String,
+    required: true
+  },
+  check: {
+    type: Boolean,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now()
+  }
 })
+
+const modelName = 'Task'
+
+export default (connection && connection.models[modelName]) ?
+(connection.models[modelName] as Model<TaskType>) : model<TaskType>(modelName, schema)
