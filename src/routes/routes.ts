644785dab7818ad79a90cbee3aadcd46fbe75c63
Gabriel Neoob <gabrielngc@gmail.com>
2023-10-23 12:08:23 -0300

    feat: routes

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -4,6 +4,15 @@ import * as TaskController from '../controller/TaskController';
 const routes = Router();
 
 routes.get('/', TaskController.getAll);
+routes.get('/todo/:id', TaskController.getById);
+
+routes.post('/create', TaskController.createTodo);
+
+routes.put('/todo/check/:id', TaskController.checkTodo);
+routes.put('/todo/update/:id', TaskController.updateTodo);
+
+routes.delete('/todo/:id',TaskController.deleteTodo);
+
 
 
 export default routes
