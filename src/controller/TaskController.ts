3d0824fd270b9b65c5fda491e8c00c1a4769ae43
Gabriel Neoob <gabrielngc@gmail.com>
2023-10-23 12:07:40 -0300

    feat: task controller

--- src/controller/TaskController.ts
+++ src/controller/TaskController.ts
@@ -1,6 +1,88 @@
 import { Request, Response } from "express";
+import Todos from '../models/Task'
 
 
+//READ
 export const getAll = async (req: Request, res: Response) => {
-  res.status(200).json({workin: true});
+  try{
+    const todos = await Todos.find();
+    res.status(202).json(todos)
+  }
+  catch(err) {
+    res.status(404).json({ error: err })
+  }
+}
+
+export const getById = async (req: Request, res: Response) => {
+  const { id } = req.params
+  try{
+    const todo = await Todos.findById(id);
+    res.status(202).json(todo)
+  }
+  catch(err) {
+    res.status(404).json({ error: err })
+  }
+}
+
+
+//CREATE
+export const createTodo = async (req: Request, res: Response) => {
+  const { task } = req.body;
+  try{
+    const newTodo = await Todos.create({
+      task,
+      check: false
+    })
+    res.status(202).json({id: newTodo._id})
+  }
+  catch(err) {
+    res.status(404).json({ error: err })
+  }
+}
+
+//PUT
+export const checkTodo = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const { check } = req.body
+  try{
+    const currentTodo = await Todos.findByIdAndUpdate(id);
+    if(currentTodo) {
+      currentTodo.check = check;
+      currentTodo.save();
+      res.status(202).json(currentTodo)
+    }
+  }
+  catch(err) {
+    res.status(404).json({ error: err })
+  }
+}
+
+export const updateTodo = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const { task } = req.body
+  try{
+    const currentTodo = await Todos.findByIdAndUpdate(id);
+    if(currentTodo) {
+      currentTodo.task = task;
+      currentTodo.save();
+      console.log(currentTodo);
+      
+      res.status(202).json(currentTodo)
+    }
+  }
+  catch(err) {
+    res.status(404).json({ error: err })
+  }
+}
+
+//DELETE
+export const deleteTodo = async (req: Request, res: Response) => { 
+  try {
+    const { id } = req.params;
+    await Todos.findByIdAndDelete(id);
+    res.status(202).json({deleted: true})
+  } 
+  catch(err) {
+    res.status(404).json({ error: err })
+  }
 }
